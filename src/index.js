75ec5815e35bfeaf33ecc3bcc1199c4564958653
Madhurshani <madhurshanisivan@gmail.com>
2021-09-20 12:44:42 +0530

    Created store.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
-
-const store=createStore(rootReducer);
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
