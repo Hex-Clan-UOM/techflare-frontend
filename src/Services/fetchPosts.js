e0af46a02c2c97bf5b1ebcd93e96b27b70bf4360
Thushitha Manokaran <manothushitha@gmail.com>
2021-10-05 14:58:07 +0530

    fixed error

--- src/Services/fetchPosts.js
+++ src/Services/fetchPosts.js
@@ -8,21 +8,18 @@ import axios from "axios";
 export const listPosts = () => async (dispatch) => {
   // const userLogin = useSelector((state) => state.userLogin);
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-  const { accessToken } = userInfo;
   try {
     dispatch(listPostRequest());
     const config = {
       headers: {
         "Content-Type": "application/json",
-        Authorization: accessToken,
+        Authorization: userInfo.accessToken,
       },
     };
-    console.log(config);
     const { data } = await axios.get(
       `${process.env.REACT_APP_URL}/posts`,
       config
     );
-    console.log(data);
 
     dispatch(listPostSuccess(data));
   } catch (error) {
