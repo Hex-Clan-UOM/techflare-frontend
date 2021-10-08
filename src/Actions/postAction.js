d973b5145bba3b3a4309213f86093703b2024a51
Madhurshani <madhurshanisivan@gmail.com>
2021-10-08 11:27:10 +0530

    Set up filtered posts redux

--- src/Actions/postAction.js
+++ src/Actions/postAction.js
@@ -2,6 +2,9 @@ import {
   POST_LIST_FAIL,
   POST_LIST_REQUEST,
   POST_LIST_SUCCESS,
+  FILTERED_POST_LIST_FAIL,
+  FILTERED_POST_LIST_REQUEST,
+  FILTERED_POST_LIST_SUCCESS,
 } from "./Types/fetchPost";
 
 export const listPostRequest = () => {
@@ -17,6 +20,25 @@ export const listPostSuccess = (data) => {
 export const listPostFail = (error) => {
   return {
     type: POST_LIST_FAIL,
+    payload:
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message,
+  };
+};
+export const listFilteredPostRequest = () => {
+  return { type: FILTERED_POST_LIST_REQUEST };
+};
+export const listFilteredPostSuccess = (data) => {
+  return {
+    type: FILTERED_POST_LIST_SUCCESS,
+    payload: data,
+  };
+};
+
+export const listFilteredPostFail = (error) => {
+  return {
+    type: FILTERED_POST_LIST_FAIL,
     payload:
       error.response && error.response.data.message
         ? error.response.data.message
