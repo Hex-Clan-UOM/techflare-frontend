e0e61c879180003964dbc24aade43225116a90ec
laroshan <laroshanlaro@gmail.com>
2021-10-13 09:52:35 +0530

    working on detailed screen

--- src/Pages/DetailedTopic/index.js
+++ src/Pages/DetailedTopic/index.js
@@ -1,28 +1,28 @@
-import {
-  Avatar,
-  Container,
-  Grid,
-  Typography,
-  CardContent,
-  Collapse,
-  Card,
-  CardHeader,
-  CardActions,
-  CardMedia,
-} from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import NavBar from "../../Components/NavBar/index";
 import { useDispatch, useSelector } from "react-redux";
 import Spinner from "../../Components/Spinner/Spinner";
 import { postDetails } from "../../Services/fetchPosts";
-import IconButton from "@mui/material/IconButton";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { red } from "@mui/material/colors";
-import { ExpandMore } from "@material-ui/icons";
+import TopicComponent from "./TopicComponent/TopicComponent";
+import RoundedBorderBtn from "../../Components/RoundedBorderBtn/RoundedBorderBtn";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    margin: "auto !important",
+  },
+  btn: {
+    marginTop: "10px !important",
+    marginBottom: "20px",
+  },
+  post: {
+    margin: "auto",
+  },
+}));
 const Index = ({ match }) => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.fetchSpecificPost);
   const { loading, error, post } = posts;
@@ -35,93 +35,23 @@ const Index = ({ match }) => {
   useEffect(() => {
     dispatch(postDetails(match.params.id));
   }, [dispatch, match]);
-  const [expanded, setExpanded] = React.useState(false);
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
+  let history = useHistory();
   return (
     <div>
       <NavBar />
       {loading ? (
         <Spinner loading={loading} size={300} />
       ) : (
-        <Container sx={{ mt: 3 }}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              avatar={<Avatar src={post.author.avatar} />}
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-              }
-              title={post.author.firstName}
-              subheader={post.createdAt.slice(0, 10)}
-            />
-
-            <CardContent>
-              <Typography
-                variant="h4"
-                color="text.secondary"
-                align="center"
-                gutterBottom
-              >
-                {post.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {post.body}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
-              <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography paragraph>Method:</Typography>
-                <Typography paragraph>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
-                </Typography>
-                <Typography paragraph>
-                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                  skillet over medium-high heat. Add chicken, shrimp and
-                  chorizo, and cook, stirring occasionally until lightly
-                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
-                  set aside, leaving chicken and chorizo in the pan. Add
-                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                  pepper, and cook, stirring often until thickened and fragrant,
-                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
-                  chicken broth; bring to a boil.
-                </Typography>
-                <Typography paragraph>
-                  Add rice and stir very gently to distribute. Top with
-                  artichokes and peppers, and cook without stirring, until most
-                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                  medium-low, add reserved shrimp and mussels, tucking them down
-                  into the rice, and cook again without stirring, until mussels
-                  have opened and rice is just tender, 5 to 7 minutes more.
-                  (Discard any mussels that don’t open.)
-                </Typography>
-                <Typography>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
+        <Container className={classes.root}>
+          <RoundedBorderBtn
+            btnText="Go Back"
+            onClick={() => {
+              history.goBack();
+            }}
+            className={classes.btn}
+          />
+          <TopicComponent post={post} className={classes.post} />
         </Container>
       )}
     </div>
