6c30cee9bd0aaa27a6c4a418cb5393e0d9c01331
laroshan <laroshanlaro@gmail.com>
2021-10-31 16:46:11 +0530

    pagination styling for mobile

--- src/Pages/DashBoard/TopicCards/style.js
+++ src/Pages/DashBoard/TopicCards/style.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   search: {
     marginBottom: 25,
     marginTop: 10,
@@ -10,10 +10,17 @@ const useStyles = makeStyles(() => ({
   },
   pagination: {
     margin: "auto",
-    padding: "1px",
     position: "absolute",
     bottom: "1px",
+    padding: "1px",
+
+    ["@media (max-width:780px)"]: {
+      // eslint-disable-line no-useless-computed-key
+      margin: "auto",
+      position: "static",
+    },
   },
+
   root: {
     minHeight: "100vh",
     position: "relative",
